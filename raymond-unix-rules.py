7f973787ff7678a145857940309de3583af63848
Michael McConville <mmcconville ~a~ mykolab.com>
2014-11-19 19:36:24 -0500

    IP address alias and quote printer

---
+++ raymond-unix-rules.py
@@ -0,0 +1,25 @@
+import random
+
+rules = ['Write simple parts connected by clean interfaces.',
+        'Clarity is better than cleverness.',
+        'Design programs to be ocnnected to other programs.',
+        'Separate policy from mechanism; separate interfaces from engines.',
+        'Design for simplicity; add complexity only where you must.',
+        'Separate policy from mechanism; separate interfaces from engines.',
+        'Design for simplicity; add complexity only where you must.',
+        'Write a big program only when it is clear by demonstration that nothing else will do.',
+        'Design for visibility to make inspection and debugging easier.',
+        'Robustness is the child of transparency and simplicity.',
+        'Fold knowledge into data so program logic can be stupid and robust.',
+        'In interface design, always do the least surprising thing.',
+        'When a program has nothing surprising to say, it should say nothing.',
+        'When you must fail, fail noisily and as soon as possible.',
+        'Programmer time is expensive; conserve it in preference to machine time.',
+        'Avoid hand-hacking; write programs to write programs when you can.',
+        'Prototype before polishing. Get it working before you optimize it.',
+        'Distrust all claims for “one true way”.',
+        'Design for the future, because it will be here sooner than you think.']
+
+print()
+print('\t' + random.choice(rules))
+print()
