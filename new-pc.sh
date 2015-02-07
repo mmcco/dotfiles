0527720a87d39931c3dcb32d347ea6d9bae1bd22
Michael McConville <mmcconville ~a~ mykolab ~d~ com>
2015-02-06 20:28:59 -0500

    more misc changes

--- new-pc.sh
+++ new-pc.sh
@@ -1,4 +1,9 @@
+# should be run with bash, not sh, because of "echo -e"
+# username assumed to be "mike"
 set -e    # causes death on error
+
+# to be run from the repo directory
+cd ~/dotfiles
 
 sudo sh -c 'echo deb     http://deb.torproject.org/torproject.org $(lsb_release -cs) main >> /etc/apt/sources.list'
 gpg --keyserver keys.gnupg.net --recv 886DDD89
@@ -7,13 +12,15 @@ gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
-sudo apt-get install git vim tmux mksh htop golang vim-addon-manager vim-syntax-go mcabber ghc hlint
+sudo apt-get install git vim tmux mksh htop golang vim-addon-manager vim-syntax-go mcabber ghc hlint gnupg2 build-essential curl
 sudo apt-get install deb.torproject.org-keyring
 sudo apt-get install tor tor-arm
 
+# tarsnap dependencies
+sudo apt-get install libssl-dev zlib1g-dev e2fslibs-dev
 
 sudo sh -c "echo '/bin/mksh' >> /etc/shells"
-sudo chsh -s /bin/mksh
+sudo -u mike chsh -s /bin/mksh
 
 ln -f .vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
@@ -24,6 +31,8 @@ vim-addon-manager install go-syntax
 ln -f .tmux.conf ~/.tmux.conf
 
 mv ~/.profile ~/.mkshrc
-echo -e "\n. .mike-profile" >> ~/.mkshrc
+echo "\n. .mike-profile" >> ~/.mkshrc
+
+ln -f raymond-rules.py ~/.raymond-rules.py
 
 cat not-done.txt
